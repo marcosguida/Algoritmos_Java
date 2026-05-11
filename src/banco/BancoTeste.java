a75041533f5b278b7462a90785b0ad9abc3e520e
Marcos Guida <105091587+marcosguida@users.noreply.github.com>
2026-05-11 19:42:40 -0300

    Add deposit, withdrawal, and purchase methods to BancoTeste

--- src/banco/BancoTeste.java
+++ src/banco/BancoTeste.java
@@ -10,18 +10,27 @@ public class BancoTeste {
 
         conta.depositar(1000.0);
         System.out.println("Saldo de R$ " + conta.consultarSaldo().toString());
+        
         conta.sacar(800.0);
         System.out.println("Saldo de R$ " + conta.consultarSaldo().toString());
+        
         conta.depositar(100.0);
+        
         conta.sacar(500.0);
+        
         System.out.println("Saldo de R$ " + conta.consultarSaldo().toString());
+        
         System.out.println("Dona(o) da Conta é " + conta.consultarTitular());
 
         conta.sacar(138.32);
         System.out.println("Saldo de R$ " + conta.consultarSaldo().toString() + "\n");
+        
         System.out.println("Primeira compra: \n");
+        
         conta.parcelarCompra(100.00, 3);
+        
         System.out.println("\n");
+        
         System.out.println("Segunda compra: \n");
         conta.parcelarCompra(232.43, 4);
 
